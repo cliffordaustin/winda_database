1a762865af88c9d76c202a2aea051f9f80f4b519
Clifford <kingsleyclifford2016@gmail.com>
2022-03-31 11:08:23 +0000

    initial commit

--- lodging/models.py
+++ lodging/models.py
@@ -21,22 +21,30 @@ TYPE_OF_STAY = (("LODGE", "LODGE"), ("HOUSE", "HOUSE"), ("UNIQUE SPACE", "UNIQUE
 class Stays(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     slug = models.SlugField(max_length=255, blank=True, null=True, editable=False)
-    name = models.CharField(max_length=250)
+    name = models.CharField(max_length=250,
+                            help_text="If no name, enter a short description of the " +
+                                      "accommodation. Eg A lovely place located at the lake side"
+                            )
     type_of_stay = models.CharField(max_length=100, choices=TYPE_OF_STAY, blank=True)
     best_describes_lodge = ArrayField(
-        models.CharField(max_length=500, blank=True, null=True), blank=True, null=True, default=list
+        models.CharField(max_length=500, blank=True, null=True), blank=True, null=True, default=list,
+        help_text="Separate each description by using ' , '. Eg Tented camp, Permanent structures"
     )
     best_describes_house = ArrayField(
-        models.CharField(max_length=500, blank=True, null=True), blank=True, null=True, default=list
+        models.CharField(max_length=500, blank=True, null=True), blank=True, null=True, default=list,
+        help_text="Separate each description by using ' , '. Eg Residential home, Villa"
     )
     best_describes_unique_space = ArrayField(
-        models.CharField(max_length=500, blank=True, null=True), blank=True, null=True, default=list
+        models.CharField(max_length=500, blank=True, null=True), blank=True, null=True, default=list,
+        help_text="Separate each description by using ' , '. Eg Boathouse, Bus"
     )
     best_describes_campsite = ArrayField(
-        models.CharField(max_length=500, blank=True, null=True), blank=True, null=True, default=list
+        models.CharField(max_length=500, blank=True, null=True), blank=True, null=True, default=list,
+        help_text="Separate each description by using ' , '. Eg On private property, In a conservancy"
     )
     best_describes_boutique_hotel = ArrayField(
-        models.CharField(max_length=500, blank=True, null=True), blank=True, null=True, default=list
+        models.CharField(max_length=500, blank=True, null=True), blank=True, null=True, default=list,
+        help_text="Separate each description by using ' , '. Eg Historical building, Resort"
     )
     location = models.CharField(max_length=350, blank=True, null=True)
     longitude = models.FloatField(blank=True, null=True)
@@ -47,7 +55,8 @@ class Stays(models.Model):
     bathrooms = models.IntegerField(blank=True, null=True)
     room_is_ensuite = models.BooleanField(default=False)
     amenities = ArrayField(
-        models.CharField(max_length=250, blank=True, null=True), blank=True, null=True, default=list
+        models.CharField(max_length=250, blank=True, null=True), blank=True, null=True, default=list,
+        help_text="Separate each amenities by using ' , '. Eg Swimming Pool, Hot tub"
     )
     description = models.TextField(blank=True, null=True)
     unique_about_place = models.TextField(blank=True, null=True)
