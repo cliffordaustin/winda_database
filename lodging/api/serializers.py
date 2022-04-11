fd6528499664218553c167e18abd2019cb2852f4
Clifford <kingsleyclifford2016@gmail.com>
2022-04-11 12:49:00 +0000

    initial commit

--- lodging/api/serializers.py
+++ lodging/api/serializers.py
@@ -20,58 +20,7 @@ class StayImageSerializer(serializers.ModelSerializer):
 class StaysSerializer(serializers.ModelSerializer):
     user = serializers.StringRelatedField(read_only=True)
     stay_images = StayImageSerializer(many=True, read_only=True)
-    user_distance = serializers.SerializerMethodField()
 
     class Meta:
         model = Stays
         fields = "__all__"
-
-    def get_user_ip(self, request):
-        x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
-
-        if x_forwarded_for:
-            ip = x_forwarded_for.split(",")[-1].strip()
-
-        else:
-            # url = "http://ipinfo.io/json"
-            # response = urlopen(url)
-            # data = json.load(response)
-            # ip = data["ip"]
-
-            ip = ""
-
-        # client_ip, _ = get_client_ip(request)
-
-        # if client_ip is None:
-        #     return ""
-
-        # else:
-        #     return client_ip
-        # host_name = socket.gethostname()
-        # ip = socket.gethostbyname(host_name)  # Get local machine ip
-
-        # url = "http://ipinfo.io/json"
-        # response = urlopen(url)
-        # data = json.load(response)
-        # ip = data["ip"]
-
-        # # ip = requests.get("https://api.ipify.org").content.decode("utf8")
-
-        # ip = u.externalipaddress()
-
-        return ip
-
-    def get_user_distance(self, obj):
-        ip = self.get_user_ip(self.context["request"])
-        # loc = geocoder.ipinfo(ip)
-        # loc = loc.latlng
-
-        # latitiude = loc[0]
-        # longitute = loc[1]
-
-        # user_loc = (latitiude, longitute)
-        # stay_loc = (obj.latitude, obj.longitude)
-
-        # return geodesic(user_loc, stay_loc).km
-
-        return ip
