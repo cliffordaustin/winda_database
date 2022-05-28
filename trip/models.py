dbe490b082cf418197281a3dfa98d42d6e7842a9
Clifford <kingsleyclifford2016@gmail.com>
2022-05-28 13:55:00 +0000

    initial commit

--- trip/models.py
+++ trip/models.py
@@ -21,6 +21,8 @@ class Trip(models.Model):
     stay = models.ForeignKey(Stays, on_delete=models.SET_NULL, null=True, blank=True)
     from_date = models.DateTimeField(default=timezone.now)
     to_date = models.DateTimeField(default=next_time)
+    number_of_people = models.IntegerField(default=1)
+    nights = models.IntegerField(default=3)
     activity = models.ForeignKey(
         Activities, on_delete=models.SET_NULL, null=True, blank=True
     )
