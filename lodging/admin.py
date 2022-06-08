84740b3d7e3665438eba023ebd4949332fa84493
cliffordaustin <cliffordaustin670@gmail.com>
2022-06-08 13:15:26 +0000

    initial commit

--- lodging/admin.py
+++ lodging/admin.py
@@ -1,5 +1,38 @@
 from django.contrib import admin
-from .models import Order, Stays, StayImage, Review, Cart
+from .models import *
+
+
+# class StayImageInline(admin.TabularInline):
+#     model = StayImage
+#     extra = 1
+
+
+# class StayAdmin(admin.ModelAdmin):
+#     inlines = (StayImageInline,)
+
+#     fieldset = (
+#         (
+#             None,
+#             {
+#                 "fields": (
+#                     "user",
+#                     "type_of_stay",
+#                     "price",
+#                     "city",
+#                     "country",
+#                     "longitude",
+#                     "latitude",
+#                     "rooms",
+#                     "beds",
+#                     "bathrooms",
+#                     "description",
+#                     "unique_about_place",
+#                     "pricing_type",
+#                     "room_is_ensuite",
+#                 )
+#             }
+#         )
+#     )
 
 
 admin.site.register(Stays)
--- lodging/models.py
+++ lodging/models.py
@@ -84,6 +84,95 @@ class Stays(models.Model):
         default=list,
         help_text="Separate each description by using ' , '. Eg Historical building, Resort",
     )
+
+    # Lodge
+    tented_camp = models.BooleanField(default=False)
+    permanent_structures = models.BooleanField(default=False)
+    part_permanent_structures = models.BooleanField(default=False)
+    mobile_camp = models.BooleanField(default=False)
+
+    # House
+    residential_home = models.BooleanField(default=False)
+    villa = models.BooleanField(default=False)
+    boathouse = models.BooleanField(default=False)
+    historical_building = models.BooleanField(default=False)
+    on_private_property = models.BooleanField(default=False)
+    cabin = models.BooleanField(default=False)
+    cottage = models.BooleanField(default=False)
+    chalets = models.BooleanField(default=False)
+    bungalow = models.BooleanField(default=False)
+    tiny_house = models.BooleanField(default=False)
+    duplex = models.BooleanField(default=False)
+    earth_house = models.BooleanField(default=False)
+    container_house = models.BooleanField(default=False)
+    terrace_house = models.BooleanField(default=False)
+
+    # Unique Space
+    bus = models.BooleanField(default=False)
+    lighthouse = models.BooleanField(default=False)
+    glasshouse = models.BooleanField(default=False)
+    treehouse = models.BooleanField(default=False)
+    barn = models.BooleanField(default=False)
+    grasshouse = models.BooleanField(default=False)
+
+    # Campsite
+    in_conservancy = models.BooleanField(default=False)
+    in_national_park = models.BooleanField(default=False)
+    in_nature_reserve = models.BooleanField(default=False)
+    in_public_area = models.BooleanField(default=False)
+
+    # Boutique Hotel
+    a_hotel_out_in_nature = models.BooleanField(default=False)
+    resort = models.BooleanField(default=False)
+    unique_achitectural_design = models.BooleanField(default=False)
+
+    # Amenities
+    swimming_pool = models.BooleanField(default=False)
+    hot_tub = models.BooleanField(default=False)
+    sauna = models.BooleanField(default=False)
+    gym = models.BooleanField(default=False)
+    patio = models.BooleanField(default=False)
+    beachfront = models.BooleanField(default=False)
+    terrace = models.BooleanField(default=False)
+    balcony = models.BooleanField(default=False)
+    firepit = models.BooleanField(default=False)
+    barbecue_grill = models.BooleanField(default=False)
+    outdoor_dining_area = models.BooleanField(default=False)
+    spa = models.BooleanField(default=False)
+    wifi = models.BooleanField(default=False)
+    parking = models.BooleanField(default=False)
+    tv = models.BooleanField(default=False)
+    air_conditioning = models.BooleanField(default=False)
+    heating = models.BooleanField(default=False)
+    kitchen = models.BooleanField(default=False)
+    laundry = models.BooleanField(default=False)
+    washing_machine = models.BooleanField(default=False)
+    dedicated_working_area = models.BooleanField(default=False)
+    smoke_alarm = models.BooleanField(default=False)
+    first_aid_kit = models.BooleanField(default=False)
+    medical_service_on_site = models.BooleanField(default=False)
+    carbon_monoxide_detector = models.BooleanField(default=False)
+    lockable_room = models.BooleanField(default=False)
+    bar = models.BooleanField(default=False)
+    restaurant = models.BooleanField(default=False)
+    giftshop = models.BooleanField(default=False)
+    photography_room = models.BooleanField(default=False)
+    themed_room = models.BooleanField(default=False)
+    pet_friendly = models.BooleanField(default=False)
+
+    # Policies
+    check_in_time = models.TimeField(blank=True, null=True)
+    check_out_time = models.TimeField(blank=True, null=True)
+    refaundable = models.BooleanField(default=False)
+    refund_policy = models.CharField(max_length=500, blank=True, null=True)
+    damage_policy = models.CharField(max_length=500, blank=True, null=True)
+    children_allowed = models.BooleanField(default=False)
+    pets_allowed = models.BooleanField(default=False)
+    smoking_allowed = models.BooleanField(default=False)
+    events_allowed = models.BooleanField(default=False)
+    covid_19_compliance = models.BooleanField(default=False)
+    covid_19_compliance_details = models.TextField(blank=True, null=True)
+
     location = models.CharField(
         max_length=350, blank=True, null=True, verbose_name="Address"
     )
