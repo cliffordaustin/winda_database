14c21f5ed24262bd2f29a95545a63a0a18778ef3
cliffordaustin <cliffordaustin670@gmail.com>
2022-07-01 22:25:56 +0000

    initial commit

--- lodging/api/pagination.py
+++ lodging/api/pagination.py
@@ -23,3 +23,14 @@ class StayPagination(PageNumberPagination):
     page_size = 8
     page_size_query_param = "page_size"
     max_page_size = 8
+
+    def get_paginated_response(self, data):
+        return Response(
+            {
+                "next": self.get_next_link(),
+                "previous": self.get_previous_link(),
+                "page_size": self.page_size,
+                "count": self.page.paginator.count,
+                "results": data,
+            }
+        )
