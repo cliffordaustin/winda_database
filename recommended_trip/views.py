ce18e8342530676ac291f0a69c3b52c32cf4033a
cliffordaustin <cliffordaustin670@gmail.com>
2022-10-17 13:46:41 +0000

    initial commit

--- recommended_trip/views.py
+++ recommended_trip/views.py
@@ -1,3 +1,24 @@
+import csv
+from django.core.exceptions import PermissionDenied
+
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import SingleTrip
 
 # Create your views here.
+
+
+def export(request):
+    if not request.user.is_staff:
+        raise PermissionDenied
+    response = HttpResponse(content_type="text/csv")
+
+    writer = csv.writer(response)
+    writer.writerow(["name", "is_active"])
+
+    for trip in SingleTrip.objects.all().values_list("name", "is_active"):
+        writer.writerow(trip)
+
+    response["Content-Dispositon"] = 'attachment; filename="trips.csv"'
+
+    return response
